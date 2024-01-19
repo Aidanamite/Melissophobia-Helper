e0619ebd83fa743e5951b592ac0dad4a09ee7f76
Aidanamite <aid.dav.rei@gmail.com>
2024-01-20 09:14:56 +1300

    Add project files.

---
+++ SilentBees/SilentBees.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using FMODUnity;
+using HMLLibrary;
+
+public class SilentBees : Mod
+{
+    Harmony harmony;
+    static public JsonModInfo modInfo;
+
+    public void Start()
+    {
+        modInfo = modlistEntry.jsonmodinfo;
+        harmony = new Harmony("com.aidanamite.SilentBees");
+        harmony.PatchAll();
+        Debug.Log(modInfo.name + " has been loaded!");
+    }
+
+    public void OnModUnload()
+    {
+        harmony.UnpatchAll();
+        Debug.Log(modInfo.name + " has been unloaded!");
+    }
+}
+
+[HarmonyPatch(typeof(BeeHive), "SetBeeAmbienceSoundActive")]
+public class Patch_BeeHiveSound
+{
+    static void Prefix(ref bool value)
+    {
+        value = false;
+    }
+}
+
+[HarmonyPatch(typeof(StudioEventEmitter), "Play")]
+public class Patch_HoneycombSound
+{
+    static bool Prefix(ref StudioEventEmitter __instance)
+    {
+        PickupItem parent = __instance.GetComponentInParent<PickupItem>();
+        if (parent != null && parent.PickupName == "Honeycomb")
+            return false;
+        if (__instance.Event == "event:/wildlife/beehive")
+            return false;
+        return true;
+    }
+}
